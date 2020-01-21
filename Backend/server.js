86a7f1b25488652bbc2f165372d4a1d9564dce2c
ajbarbati <ajbarbati@gmail.com>
2020-01-21 15:55:16 -0500

    added app.set netlify views folder

--- Backend/server.js
+++ Backend/server.js
@@ -31,9 +31,10 @@ const hbs = handlebars.create({
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
-app.set('views', path.join(__dirname, '../views'))
+// app.set('views', path.join(__dirname, '../views'))
+app.set('views', path.join(__dirname, '/.netlify/functions/views'))
 app.use(express.static('../public/'))
-app.use('/.netlify/functions/server', hbs.engine)
+
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
